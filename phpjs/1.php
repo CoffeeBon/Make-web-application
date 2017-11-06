f4591f224bb6c8560bf57fcdc1af0eb2ee556a38
coffeebon <blue3197@gmail.com>
2017-11-06 17:47:59 +0900

    php Lesson 1

---
+++ phpjs/1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>php</h1>
+  
+  <?php 
+    echo 10+10; 
+    ?>
+<h1>JavaScript</h1>
+    <script>
+        document.write(10+10);
+    </script>
+</body>
+
+</html>
