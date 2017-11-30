a49edf87b830eb7929bb3f0bf2db916e54e00c11
coffeebon <blue3197@gmail.com>
2017-11-30 21:26:47 +0900

    login php

--- phpjs/8-2.php
+++ phpjs/8-2.php
@@ -6,7 +6,13 @@
 <body>
   <?php
     // echo 사용자가 입력한 정보중에 password의 값
-    $_GET["password"];
+    $password = $_GET["password"];
+    if ($password == "1111") {
+      echo "주인님 환영합니다.";
+    }
+    else {
+      echo "뉘신지?";
+    }
    ?>
 </body>
 </html>
